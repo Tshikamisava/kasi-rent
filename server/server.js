0ea9366ffd421438e93f7311611baaaa1ca4a5b0
Clarence289 <sitholeclarence9@gmail.com>
2026-04-15 17:11:04 +0200

    fix: bind server to 0.0.0.0 for Render deployment

--- server/server.js
+++ server/server.js
@@ -158,7 +158,7 @@ const startServer = async () => {
     process.exit(1);
   });
 
-  server.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+  server.listen(PORT, '0.0.0.0', () => console.log(`🚀 Server running on port ${PORT}`));
 };
 
 startServer();
